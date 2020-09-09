b5990d38ebfea0b305682e88e048faebdfbcb280
kulakova_tt <kulakova.tatyana12@gmail.com>
2020-09-09 20:04:22 +0300

    sfgssd

---
+++ test git/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace test_git
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
